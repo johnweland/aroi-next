a4f298549b731dc6fa3b6684d34b0d8b1b7cbd08
John Weland <john.weland@gmail.com>
2022-03-23 17:03:10 -0500

    began navigation and darkmode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -67,9 +67,9 @@ const NavBar = () => {
     }
 
     return (
-        <Popover className="relative bg-white dark:bg-gray-900">
+        <Popover className="relative bg-white dark:bg-neutral-900">
             <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                <div className="flex justify-between items-center border-b-2 border-gray-100 dark:border-neutral-800 py-6 md:justify-start md:space-x-10">
                     <div className="flex justify-start lg:w-0 lg:flex-1">
                         <a href="#">
                             <span className="sr-only">Workflow</span>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-white dark:bg-gray-900">
+      <body className="bg-white dark:bg-neutral-900">
         <Main />
         <NextScript />
       </body>
