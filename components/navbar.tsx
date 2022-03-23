bb9e9bfd4e6d02e1fcdb966fea1c80f53d52a5e4
John Weland <john.weland@gmail.com>
2022-03-23 17:10:53 -0500

    darkmode tweaks

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -72,7 +72,7 @@ const NavBar = () => {
                 <div className="flex justify-between items-center border-b-2 border-gray-100 dark:border-neutral-800 py-6 md:justify-start md:space-x-10">
                     <div className="flex justify-start lg:w-0 lg:flex-1">
                         <a href="#">
-                            <span className="sr-only">Workflow</span>
+                            <span className="sr-only">Aroi Thai Cuisine</span>
                             <img
                                 className="h-8 w-auto sm:h-10"
                                 src="https://tailwindui.com/img/logos/workflow-mark-orange-600.svg"
@@ -81,7 +81,7 @@ const NavBar = () => {
                         </a>
                     </div>
                     <div className="-mr-2 -my-2 md:hidden">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
+                        <Popover.Button className="bg-white dark:bg-neutral-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
                             <span className="sr-only">Open menu</span>
                             <MenuIcon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -125,18 +125,18 @@ const NavBar = () => {
                 leaveTo="opacity-0 scale-95"
             >
                 <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white dark:bg-neutral-800 divide-y-2 divide-gray-50">
                         <div className="pt-5 pb-6 px-5">
                             <div className="flex items-center justify-between">
                                 <div>
                                     <img
                                         className="h-8 w-auto"
                                         src="https://tailwindui.com/img/logos/workflow-mark-orange-600.svg"
-                                        alt="Workflow"
+                                        alt="Aroi Thai Cuisine"
                                     />
                                 </div>
                                 <div className="-mr-2">
-                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
+                                    <Popover.Button className="bg-white dark:bg-neutral-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
                                         <span className="sr-only">Close menu</span>
                                         <XIcon className="h-6 w-6" aria-hidden="true" />
                                     </Popover.Button>
@@ -151,7 +151,7 @@ const NavBar = () => {
                                             className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                                         >
                                             <item.icon className="flex-shrink-0 h-6 w-6 text-orange-600" aria-hidden="true" />
-                                            <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                                            <span className="ml-3 text-base font-medium text-gray-900 dark:text-gray-100">{item.name}</span>
                                         </a>
                                     ))}
                                 </nav>
